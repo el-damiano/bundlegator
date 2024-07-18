91d6624104093fe85f13aceb915074bd816e799b
el-damiano <el9damiano@gmail.com>
2024-07-18 21:22:30 +0200

    fix(Bundle): change direct subscripts to None-safe get functions

--- src/Bundle.py
+++ src/Bundle.py
@@ -6,21 +6,23 @@ class Bundle:
 
     def __init__(self, source: Source, document: dict):
         if source is Source.HUMBLE_BUNDLE:
-            self.name = "Humble Bundle: " + document["bundleData"]["basic_data"]["human_name"] or "Unknown"
-            self.author = document.get("bundleData", {}).get("author") or "Unkown"
-            self.url = source + document.get("bundleData", {}).get("page_url") or "/bundles"
+            bundle_data = document.get("bundleData", {})
+            basic_data = bundle_data.get("basic_data", {})
+            self.name = basic_data.get("human_name") or "Unknown"
+            self.author = bundle_data.get("author") or "Unkown"
+            self.url = source + "/" + (bundle_data.get("page_url") or "bundles")
             self.start_date = document.get("at_time|datetime") or "Unkown"
-            self.end_date = str(document.get("bundleData", {}).get("basic_data", {}).get("end_time_datetime")) or "Unknown"
+            self.end_date = str(basic_data.get("end_time_datetime")) or "Unknown"
             self.charity = None
-            self.total_price = None
+            self.total_price = basic_data.get("msrp|money") or "Unknown"
             self.items = None  # dict item_by_tier
         elif source is Source.FANATICAL:
-            self.name = "Fanatical: " + document["name"] or "Unkown"
+            self.name = document.get("name") or "Unkown"
             self.author = "Fanatical"
             self.url = source + "/" + (document.get("type") or "en") + "/" + (document.get("slug") or "bundle")
             unix_time = document.get("release_date")
             self.start_date = str(datetime.fromtimestamp(unix_time)) if unix_time else "Unkown"
             self.end_date = "Unkown"
-            self.charity = None
-            self.total_price = None
+            self.charity = "None"
+            self.total_price = document.get("fullPrice", {}).get("EUR") or "Unknown"
             self.items = None  # dict item_by_tier
