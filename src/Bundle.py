8dcc034f472b4db655bbfcc96f64692f4bd30e95
el-damiano <el9damiano@gmail.com>
2024-07-18 20:31:35 +0200

    !fix(Bundle): get 'em getters outta here!

--- src/Bundle.py
+++ src/Bundle.py
@@ -5,42 +5,22 @@ from datetime import datetime
 class Bundle:
 
     def __init__(self, source: Source, document: dict):
-        self.name = self.get_name(source, document)
-        self.author = self.get_author(source, document)
-        self.url = self.get_url(source, document)
-        self.start_date = None
-        self.end_date = None
-        self.charity = None
-        self.total_price = None
-        self.items = None  # dict item_by_tier
-
-    def get_name(self, source: Source, document: dict) -> str:
-        if source == Source.HUMBLE_BUNDLE:
-            return "Humble Bundle: " + document["bundleData"]["basic_data"]["human_name"] or "Unknown"
-        elif source == Source.FANATICAL:
-            return "Fanatical: " + document["name"] or "Unkown"
-
-    def get_author(self, source: Source, document: dict) -> str:
-        if source == Source.HUMBLE_BUNDLE:
-            return document.get("bundleData", {}).get("author") or "Unkown"
-        elif source == Source.FANATICAL:
-            return "Fanatical: " + document["name"] or "Unkown"
-
-    def get_url(self, source: Source, document: dict) -> str:
-        if source == Source.HUMBLE_BUNDLE:
-            return Source.HUMBLE_BUNDLE + document.get("bundleData", {}).get("page_url") or "/bundles"
-        elif source == Source.FANATICAL:
-            return f"{Source.FANATICAL}/{document.get("type") or "en"}/{document.get("slug") or "bundle"}"
-
-    def get_start_date(self, source: Source, document: dict) -> str:
-        if source == Source.HUMBLE_BUNDLE:
-            return document.get("at_time|datetime") or "Unkown"
-        elif source == Source.FANATICAL:
+        if source is Source.HUMBLE_BUNDLE:
+            self.name = "Humble Bundle: " + document["bundleData"]["basic_data"]["human_name"] or "Unknown"
+            self.author = document.get("bundleData", {}).get("author") or "Unkown"
+            self.url = source + document.get("bundleData", {}).get("page_url") or "/bundles"
+            self.start_date = document.get("at_time|datetime") or "Unkown"
+            self.end_date = str(document.get("bundleData", {}).get("basic_data", {}).get("end_time_datetime")) or "Unknown"
+            self.charity = None
+            self.total_price = None
+            self.items = None  # dict item_by_tier
+        elif source is Source.FANATICAL:
+            self.name = "Fanatical: " + document["name"] or "Unkown"
+            self.author = "Fanatical"
+            self.url = source + "/" + (document.get("type") or "en") + "/" + (document.get("slug") or "bundle")
             unix_time = document.get("release_date")
-            return str(datetime.fromtimestamp(unix_time)) if unix_time else "Unkown"
-
-    def get_end_date(self, source: Source, document: dict) -> str:
-        if source == Source.HUMBLE_BUNDLE:
-            return str(document.get("bundleData", {}).get("basic_data", {}).get("end_time_datetime")) or "Unknown"
-        elif source == Source.FANATICAL:
-            return "Unkown"
+            self.start_date = str(datetime.fromtimestamp(unix_time)) if unix_time else "Unkown"
+            self.end_date = "Unkown"
+            self.charity = None
+            self.total_price = None
+            self.items = None  # dict item_by_tier
