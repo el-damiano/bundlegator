19211ae4bb1aaa3d007afcb4991b68e60e0224f2
el-damiano <el9damiano@gmail.com>
2024-07-23 17:29:10 +0200

    feat(RSS): init

---
+++ src/RSS.py
@@ -0,0 +1,67 @@
+from Bundle import Bundle, BundleItem
+
+
+class Channel:
+
+    def __init__(self, bundles: list[Bundle]) -> None:
+        self.title = "Various Bundles"
+        self.link = "https://www.humblebundle.com/bundles"
+        self.description = "Bundles that got scraped from various websites"
+        self.items = list(map(
+            lambda bundle: ChannelItem(bundle),
+            bundles,
+        ))
+
+    def __repr__(self) -> str:
+        return f"""<rss version="2.0">
+    <channel>
+    <title>{self.title}</title>
+    <link>{self.link}</link>
+    <description>{self.description}</description>
+    {[item for item in self.items]}
+    </channel>
+</rss> """
+
+
+class ChannelItem:
+
+    def __init__(self, bundle: Bundle) -> None:
+        self.title = bundle.name
+        self.link = bundle.url
+
+        self.author = None
+        self.pubDate = None
+        self.description = bundle.items
+
+    def __repr__(self) -> str:
+        return f"""
+<item>
+    <title>{self.title}</title>
+    <link>{self.link}</link>
+    <guid>{self.link}</guid>
+    <pubDate>{self.pubDate}</pubDate>
+    <description><![CDATA[
+        <br>
+        {self.description}
+        <br>
+    ]]></description>
+</item>
+        """
+
+
+class ChannelDescription:
+
+    def __init__(self, item: BundleItem) -> None:
+        self.name = item.name
+        self.authors = item.authors
+        self.publisher = item.publisher
+        self.platforms = item.platforms
+        self.isbn = item.isbn
+        self.release_date = item.release_date
+
+    def __repr__(self) -> str:
+        return f"""
+            {self.name}
+            {self.authors}
+
+"""
